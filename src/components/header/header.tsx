54096a1ce38cbe29cd4e4ed5aa4224d355a40640
Stanislav Belov <mshmastermind@gmail.com>
2023-10-05 17:24:09 +0500

    search test1

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { AppRoute } from '../../utils/const';
+import SearchForm from '../search-form/search-form';
 
 function Header() : JSX.Element {
   return (
@@ -39,49 +40,7 @@ function Header() : JSX.Element {
             </li>
           </ul>
         </nav>
-        <div className="form-search">
-          <form>
-            <label>
-              <svg
-                className="form-search__icon"
-                width={16}
-                height={16}
-                aria-hidden="true"
-              >
-                <use xlinkHref="#icon-lens" />
-              </svg>
-              <input
-                className="form-search__input"
-                type="text"
-                autoComplete="off"
-                placeholder="Поиск по сайту"
-              />
-            </label>
-            <ul className="form-search__select-list">
-              <li className="form-search__select-item" tabIndex={0}>
-              Cannonball Pro MX 8i
-              </li>
-              <li className="form-search__select-item" tabIndex={0}>
-              Cannonball Pro MX 7i
-              </li>
-              <li className="form-search__select-item" tabIndex={0}>
-              Cannonball Pro MX 6i
-              </li>
-              <li className="form-search__select-item" tabIndex={0}>
-              Cannonball Pro MX 5i
-              </li>
-              <li className="form-search__select-item" tabIndex={0}>
-              Cannonball Pro MX 4i
-              </li>
-            </ul>
-          </form>
-          <button className="form-search__reset" type="reset">
-            <svg width={10} height={10} aria-hidden="true">
-              <use xlinkHref="#icon-close" />
-            </svg>
-            <span className="visually-hidden">Сбросить поиск</span>
-          </button>
-        </div>
+        <SearchForm />
         <a className="header__basket-link" href="#">
           <svg width={16} height={16} aria-hidden="true">
             <use xlinkHref="#icon-basket" />
---
+++ src/components/search-form/search-form.tsx
@@ -0,0 +1,91 @@
+import { useAutocomplete } from '@mui/base/useAutocomplete';
+
+import { useAppSelector } from '../../hooks';
+import { getCameras } from '../../store/cameras-data/cameras-data.selectors';
+import { useNavigate } from 'react-router-dom';
+import { AppRoute } from '../../utils/const';
+import './search-form.css';
+
+
+export default function SearchForm () : JSX.Element {
+
+  const searchOptions = useAppSelector(getCameras);
+  const navigate = useNavigate();
+
+  const handleOptionClick = (id : number | undefined) => {
+    if(id) {
+      navigate(`${AppRoute.Root}${id?.toString()}`);
+    }
+
+  };
+
+  const {getRootProps, groupedOptions , getOptionProps, getInputProps , getInputLabelProps, getListboxProps, popupOpen , getClearProps, inputValue} = useAutocomplete({
+    id: 'header-search-bar',
+    options: searchOptions,
+    getOptionLabel: (option) => option.name,
+    handleHomeEndKeys: true,
+    clearOnEscape: true,
+    onChange(evt, value) {
+      evt.preventDefault();
+      handleOptionClick(value?.id);
+    },
+    filterOptions(options, state) {
+      if (state.inputValue.length > 2) {
+        return options.filter((item) =>
+          String(item.name).toLowerCase().includes(state.inputValue.toLowerCase())
+        );
+      }
+      return options;
+    },
+  });
+
+  return (
+    <div {...getRootProps()} className={`form-search ${popupOpen ? 'list-opened' : ''}`} >
+      <form >
+        <label {...getInputLabelProps()}>
+          <svg
+            className="form-search__icon"
+            width={16}
+            height={16}
+            aria-hidden="true"
+          >
+            <use xlinkHref="#icon-lens" />
+          </svg>
+          <input
+            className="form-search__input"
+            type="text"
+            autoComplete="off"
+            placeholder="Поиск по сайту"
+            {...getInputProps()}
+          />
+        </label>
+        <ul {...getListboxProps()} className="form-search__select-list">
+          {groupedOptions.length > 0 ? (groupedOptions as typeof searchOptions).map((option, index) => (
+            <li
+              {...getOptionProps({option, index})}
+              key={option.name}
+              className="form-search__select-item"
+              tabIndex={0}
+            >
+              {option.name}
+            </li>
+          ))
+            :
+            <li
+              className="form-search__select-item"
+              tabIndex={0}
+            >
+              По запросу ничего не найдено...
+            </li>}
+        </ul>
+      </form>
+      {inputValue !== '' &&
+      <button {...getClearProps()} className="form-search__reset" type="reset">
+        <svg width={10} height={10} aria-hidden="true">
+          <use xlinkHref="#icon-close" />
+        </svg>
+        <span className="visually-hidden">Сбросить поиск</span>
+      </button>}
+    </div>
+  );
+}
